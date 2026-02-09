f70674918d16c24fb4d5cdc0b91cce9b80dd53ba
Yannawut-551201007 <wit.yannawut@gmail.com>
2026-02-09 15:56:21 +0700

    Add outputs for VPC and RDS instance details

---
+++ infra/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "rds_port" {
+  value = aws_db_instance.postgres.port
+}
+
+output "db_name" {
+  value = aws_db_instance.postgres.db_name
+}
+
+output "db_username" {
+  value = aws_db_instance.postgres.username
+}
+
+output "db_password" {
+  value     = random_password.db.result
+  sensitive = true
+}
